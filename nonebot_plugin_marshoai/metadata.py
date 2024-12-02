a9b0ba42cc98e3cd6b6bfb61f871e2ea5e76d584
Asankilp <asankilp@outlook.com>
2024-12-02 13:27:45 +0800

    更改description

--- nonebot_plugin_marshoai/metadata.py
+++ nonebot_plugin_marshoai/metadata.py
@@ -5,7 +5,7 @@ from .constants import USAGE
 
 metadata = PluginMetadata(
     name="Marsho AI插件",
-    description="接入Azure服务或其他API的AI猫娘聊天插件",
+    description="接入Azure服务或其他API的AI猫娘聊天插件，支持图片处理，外部函数调用，兼容多个AI模型，可解析AI回复的富文本信息",
     usage=USAGE,
     type="application",
     config=ConfigModel,
