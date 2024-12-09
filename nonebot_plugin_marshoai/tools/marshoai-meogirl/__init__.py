4ca5324f69dfc67f6ef64ea56a6417597c1747f0
Twisuki <Twisuki@outlook.com>
2024-12-10 00:30:13 +0800

    new file:   nonebot_plugin_marshoai/tools/marshoai-meogirl/__init__.py 	new file:   nonebot_plugin_marshoai/tools/marshoai-meogirl/mg_Info.py 	new file:   nonebot_plugin_marshoai/tools/marshoai-meogirl/tools.json

---
+++ nonebot_plugin_marshoai/tools/marshoai-meogirl/__init__.py
@@ -0,0 +1,4 @@
+from . import mg_Info
+
+async def meogirl():
+    return mg_Info.meogirl()
---
+++ nonebot_plugin_marshoai/tools/marshoai-meogirl/mg_Info.py
@@ -0,0 +1,4 @@
+
+# Meogirl
+def meogirl():
+    return "Meogirl指的是\"萌娘百科\"(https://zh.moegirl.org.cn/ , 简称\"萌百\"), 是一个\"万物皆可萌的百科全书！\"; 同时, MarshoTools也配有\"Meogirl\"插件, 可调用萌百的api"
