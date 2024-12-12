1a34e9b167f94a3e2b7aac7c70d9a707eaccedde
Nya_Twisuki <100500860+Twisuki@users.noreply.github.com>
2024-12-13 00:34:50 +0800

    新增 Introduce 功能, 将其从 Search 分离 (#25)
    * 修复了style和script标签无法去除的问题
    
    * 添加了图片展示 & 统一了引号格式
    
    * 添加了图片展示 & 统一了引号格式
    
    * 统一引号格式并异步处理函数
    
    * 新增了Introduce功能
    
    * 新增了Introduce功能并将其从Search中分离

--- nonebot_plugin_marshoai/tools/marshoai-meogirl/__init__.py
+++ nonebot_plugin_marshoai/tools/marshoai-meogirl/__init__.py
@@ -1,10 +1,16 @@
+
 from . import mg_Info
 from . import mg_Search
+from . import mg_Introduce
 
 # meogirl
-async def meogirl():
+async def meogirl () :
     return mg_Info.meogirl()
 
 # Search
-async def search(msg : str, num : int = 3):
+async def search (msg : str, num : int = 3) :
     return str(await mg_Search.search(msg, num))
+
+# Show
+async def introduce (msg : str) :
+    return str(await mg_Introduce.introduce(msg))
---
+++ nonebot_plugin_marshoai/tools/marshoai-meogirl/mg_Introduce.py
@@ -0,0 +1,76 @@
+from nonebot.log import logger
+
+import re
+import httpx
+import urllib.parse
+from bs4 import BeautifulSoup
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
+}
+
+async def get_async_data (url) :
+    async with httpx.AsyncClient(timeout = None) as client:
+        return await client.get(url, headers = headers)
+
+async def introduce (msg : str) :
+    logger.info(f"介绍 : \"{msg}\" ...")
+    result = ""
+
+    url = "https://mzh.moegirl.org.cn/" + urllib.parse.quote_plus(msg)
+    response = await get_async_data(url)
+    logger.success(f"连接\"{url}\"完成, 状态码 : {response.status_code}")
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # 正常页
+    if response.status_code == 200 :
+        """
+            萌娘百科页面结构
+            div#mw-content-text
+            └── div#404search           # 空白页面出现
+            └── div.mw-parser-output    # 正常页面
+                └── div, p, table ...   # 大量的解释项
+        """
+        result += msg + "\n"
+
+        img = soup.find("img", class_="infobox-image")
+        if img:
+            result += f"![ {msg} ]( {img["src"]} ) \n"
+
+        div = soup.find("div", class_="mw-parser-output")
+        if div:
+            p_tags = div.find_all("p")
+            num = 0
+            for p_tag in p_tags:
+                p = str(p_tag)
+                p = re.sub(r"<script.*?</script>|<style.*?</style>", "", p, flags=re.DOTALL)
+                p = re.sub(r"<.*?>", "", p, flags=re.DOTALL)
+                p = re.sub(r"\[.*?]", "", p, flags=re.DOTALL)
+
+                if p != "":
+                    result += str(p)
+
+                    num += 1
+                    if num >= 20:
+                        break
+        return result
+
+    # 空白页
+    elif response.status_code == 404 :
+        logger.info(f"未找到\"{msg}\", 进行搜索")
+
+        from . import mg_Search
+        context = await mg_Search.search(msg, 1)
+        keyword = re.search(r".*?\n", context, flags = re.DOTALL).group()[: -1]
+
+        logger.success(f"搜索完成, 打开\"{keyword}\"")
+        return await introduce(keyword)
+
+    # 搜索失败
+    elif response.status_code == 301 :
+        return f"未找到{msg}"
+
+    else :
+        logger.error(f"网络错误, 状态码 : {response.status_code}")
+        return f"网络错误, 状态码 : {response.status_code}"
