e179543742f9ebe21e2c128741b07ef88d914576
Snowykami <snowykami@outlook.com>
2024-12-13 03:31:58 +0800

    ✨ 添加 mk_common.py 和 mk_info.py 文件，提供随机转盘和数字计算功能

---
+++ nonebot_plugin_marshoai/tools/marshoai_megakits/mk_info.py
@@ -0,0 +1,8 @@
+# Twisuki
+async def twisuki():
+    return 'Twiuski(苏阳)是megakits插件作者, Github : "https://github.com/Twisuki"'
+
+
+# MegaKits
+async def megakits():
+    return 'MegaKits插件是一个功能混杂的MarshoAI插件, 由Twisuki(Github : "https://github.com/Twisuki")开发, 插件仓库 : "https://github.com/LiteyukiStudio/marsho-toolsets/tree/main/Twisuki/marshoai-megakits"'
