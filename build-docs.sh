448758d40dce38f02e0d9293e15465c3240b7914
Snowykami <snowykami@outlook.com>
2024-12-14 16:25:05 +0800

    ✨ 重构文档构建流程，将 litedoc 命令移至 build-docs.sh 脚本中，并赋予执行权限

---
+++ build-docs.sh
@@ -0,0 +1,2 @@
+litedoc nonebot_plugin_marshoai -o docs/zh/dev/api -l zh-Hans    -cd class -fd func -md func -vd var -bu https://github.com/LiteyukiStudio/nonebot-plugin-marshoai/tree/main/nonebot_plugin_marshoai/
+litedoc nonebot_plugin_marshoai -o docs/en/dev/api -l en         -cd class -fd func -md func -vd var -bu https://github.com/LiteyukiStudio/nonebot-plugin-marshoai/tree/main/nonebot_plugin_marshoai/
