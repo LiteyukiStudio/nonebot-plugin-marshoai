ec15504ae23e3ce657761e3e321ef60e56e2be18
Twisuki <Twisuki@outlook.com>
2024-12-18 01:40:30 +0800

    新增了养猫插件

---
+++ nonebot_plugin_marshoai/plugins/twisuki_petcat/__init__.py
@@ -0,0 +1,13 @@
+from nonebot_plugin_marshoai.plugin import (
+    Integer,
+    Parameter,
+    PluginMetadata,
+    String,
+    on_function_call,
+)
+
+__marsho_meta__ = PluginMetadata(
+    name="养猫插件",
+    description="在Marsho这里赛博养猫",
+    author="Twisuki",
+)
