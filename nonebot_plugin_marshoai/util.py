c8dd126042f10347c2098a1062ecf652a181b440
Asankilp <asankilp@outlook.com>
2024-12-29 16:33:25 +0800

    🐛 移除make_chat函数中的tool_choice参数以简化函数调用

--- nonebot_plugin_marshoai/util.py
+++ nonebot_plugin_marshoai/util.py
@@ -95,7 +95,6 @@ async def make_chat(
         messages=msg,
         model=model_name,
         tools=tools,
-        tool_choice="auto",
         temperature=config.marshoai_temperature,
         max_tokens=config.marshoai_max_tokens,
         top_p=config.marshoai_top_p,
