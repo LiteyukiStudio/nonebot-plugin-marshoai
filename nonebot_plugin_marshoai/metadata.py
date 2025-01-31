887bf808a7f74aa6e46460fe517bbe6e37732040
Asankilp <asankilp@outlook.com>
2025-01-31 16:35:59 +0800

    修改元数据和gitignore

--- nonebot_plugin_marshoai/metadata.py
+++ nonebot_plugin_marshoai/metadata.py
@@ -4,8 +4,8 @@ from .config import ConfigModel
 from .constants import USAGE
 
 metadata = PluginMetadata(
-    name="Marsho AI插件",
-    description="接入Azure服务或其他API的AI猫娘聊天插件，支持图片处理，外部函数调用，兼容多个AI模型，可解析AI回复的富文本信息",
+    name="Marsho AI 插件",
+    description="接入 Azure API 或其他 API 的 AI 聊天插件，支持图片处理，外部函数调用，兼容包括 DeepSeek-R1 在内的多个模型",
     usage=USAGE,
     type="application",
     config=ConfigModel,
